9d3acfc21ba6fb98726448bf74aecc76156f440e
alireza <alireza.daghigh1999@gmail.com>
2020-07-30 01:06:15 +0430

    menu completed

---
+++ JormYab/app/src/main/java/com/example/jormyab/MenuActivity.java
@@ -0,0 +1,42 @@
+package com.example.jormyab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    ImageView profile;
+    ImageView map;
+    ImageView reportCrime;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_activity);
+        findViews();
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuActivity.this , ProfileActivity.class);
+                startActivity(i);
+
+            }
+        });
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuActivity.this , MapsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void findViews(){
+        profile = findViewById(R.id.profile_image);
+        map = findViewById(R.id.map_image);
+        reportCrime = findViewById(R.id.report_image);
+    }
+}
--- JormYab/app/src/main/java/com/example/jormyab/ProfileActivity.java
+++ JormYab/app/src/main/java/com/example/jormyab/ProfileActivity.java
@@ -11,16 +11,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class ProfileActivity extends Fragment {
-    SharedPreferences setting;
-
-    @Nullable
+public class ProfileActivity extends AppCompatActivity {
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_fragment);
     }
 }
