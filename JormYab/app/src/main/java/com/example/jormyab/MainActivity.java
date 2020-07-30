49e4fc52c52624efff7aa46342db53af1ba99fd7
hassan <h.zaker2222@gmail.com>
2020-07-31 01:08:47 +0430

    vibration set for alerting

--- JormYab/app/src/main/java/com/example/jormyab/MainActivity.java
+++ JormYab/app/src/main/java/com/example/jormyab/MainActivity.java
@@ -1,23 +1,38 @@
 package com.example.jormyab;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+
 import androidx.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences setting;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setting = PreferenceManager.getDefaultSharedPreferences(this);
+        long[] timing = new long[]{500, 500, 500, 200, 500, 500, 500, 500, 500, 200, 500, 500};
+        int[] strength = new int[]{255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0};
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(VibrationEffect.createWaveform(timing, strength, -1));
+//        Intent i = new Intent(this, AlertDangerService.class);
+//        System.out.println("hi");
+//        startService(i);
+//        System.out.println("bye");
+//        setting = PreferenceManager.getDefaultSharedPreferences(this);
         if (true) {
-            Intent i = new Intent(MainActivity.this, MenuActivity.class);
+            Intent i = new Intent(MainActivity.this, MapsActivity.class);
             startActivity(i);
             MainActivity.this.finish();
         }
@@ -26,6 +41,6 @@ public class MainActivity extends AppCompatActivity {
 //            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container , profileActivity).addToBackStack(null).commit();
             SubmitCrimeFragment submitCrimeFragment = new SubmitCrimeFragment(this);
             submitCrimeFragment.show();
-        };
+        }
     }
 }
