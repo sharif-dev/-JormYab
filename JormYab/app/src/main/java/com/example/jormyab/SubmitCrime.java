b23f3cac5e24b5ff478b5f6646339d6dd5e55048
aliabbasi7798 <aliabbasi7898@gmail.com>
2020-08-01 03:09:09 +0430

    SubmitCrime class

---
+++ JormYab/app/src/main/java/com/example/jormyab/SubmitCrime.java
@@ -0,0 +1,18 @@
+package com.example.jormyab;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubmitCrime extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.submit_crime, container, false);
+    }
+}
