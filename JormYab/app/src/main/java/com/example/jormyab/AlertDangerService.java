30f8defbc0eb8630e4386081a17828ed2d64ed41
alireza <alireza.daghigh1999@gmail.com>
2020-07-31 22:17:11 +0430

    add lock or unlock checker that i don't know right or wrong

--- JormYab/app/src/main/java/com/example/jormyab/AlertDangerService.java
+++ JormYab/app/src/main/java/com/example/jormyab/AlertDangerService.java
@@ -2,6 +2,8 @@ package com.example.jormyab;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.KeyguardManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -15,11 +17,14 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.concurrent.ExecutionException;
 
@@ -33,14 +38,19 @@ public class AlertDangerService extends Service {
     private LocationListener locationListener;
 
 
+
     private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
     private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        final boolean[] isLocked = new boolean[1];
+        final View view = View.inflate(getApplicationContext(), R.layout.activity_main, null);
+
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
+            @SuppressLint("ResourceType")
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onLocationChanged(Location location) {
@@ -48,11 +58,22 @@ public class AlertDangerService extends Service {
                 try {
                     boolean response = new Client2().execute(s).get();
                     if (response){
+                        KeyguardManager myKM = (KeyguardManager) getApplicationContext().getSystemService(Context.KEYGUARD_SERVICE);
+                        assert myKM != null;
+                        isLocked[0] = myKM.inKeyguardRestrictedInputMode();
+
                         Toast.makeText(getBaseContext(),s, Toast.LENGTH_LONG).show();
-                        long[] timing = new long[]{500, 500, 500, 200, 500, 500, 500, 500, 500, 200, 500, 500};
-                        int[] strength = new int[]{255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0};
-                        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                        vibrator.vibrate(VibrationEffect.createWaveform(timing, strength, -1));
+                        if (isLocked[0]) {
+                            long[] timing = new long[]{500, 500, 500, 200, 500, 500, 500, 500, 500, 200, 500, 500};
+                            int[] strength = new int[]{255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0};
+                            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(VibrationEffect.createWaveform(timing, strength, -1));
+                        }
+                        else {
+                            Snackbar.make(view.findViewById(R.layout.popup_snackbar), "danger place",
+                                    Snackbar.LENGTH_SHORT)
+                                    .show();
+                        }
                         //TODO notification
                     }else {
                         Toast.makeText(getBaseContext(),"OK", Toast.LENGTH_LONG).show();
--- JormYab/app/src/main/java/com/example/jormyab/MainActivity.java
+++ JormYab/app/src/main/java/com/example/jormyab/MainActivity.java
@@ -12,27 +12,30 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.view.View;
 
 import androidx.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences setting;
+    public static View view;
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        long[] timing = new long[]{700, 500, 200, 40, 700, 500, 200, 50, 200, 50, 200, 50,200,50,200,50,700,50};
-        int[] strength = new int[]{255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0,255,0,255,0,255,0};
-        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        vibrator.vibrate(VibrationEffect.createWaveform(timing, strength, -1));
+//        long[] timing = new long[]{700, 500, 200, 40, 700, 500, 200, 50, 200, 50, 200, 50,200,50,200,50,700,50};
+//        int[] strength = new int[]{255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0,255,0,255,0,255,0};
+//        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+//        vibrator.vibrate(VibrationEffect.createWaveform(timing, strength, -1));
 //        Intent i = new Intent(this, AlertDangerService.class);
 //        System.out.println("hi");
 //        startService(i);
 //        System.out.println("bye");
 //        setting = PreferenceManager.getDefaultSharedPreferences(this);
         if (true) {
-            Intent i = new Intent(MainActivity.this, MapsActivity.class);
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(i);
             MainActivity.this.finish();
         }
