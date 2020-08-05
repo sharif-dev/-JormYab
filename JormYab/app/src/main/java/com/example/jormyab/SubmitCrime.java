48c519a8cf32cb62ac04caab4cc70bbc1e4461a8
aliabbasi7798 <aliabbasi7898@gmail.com>
2020-08-05 18:09:37 +0430

    Time set

--- JormYab/app/src/main/java/com/example/jormyab/SubmitCrime.java
+++ JormYab/app/src/main/java/com/example/jormyab/SubmitCrime.java
@@ -1,11 +1,13 @@
 package com.example.jormyab;
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -44,26 +47,31 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class SubmitCrime extends Fragment  implements AdapterView.OnItemSelectedListener {
     private Button location;
     private Button submit;
     private TextView welcome;
-    private Spinner hour;
-    private Spinner day;
-    private Spinner month;
-    private Spinner year;
+   // private Spinner hour;
+   // private Spinner day;
+   // private Spinner month;
+  //  private Spinner year;
     private Spinner crime;
     private TextInputEditText other;
     private Context thisContext;
     private String otherStr;
-    private String hourStr;
-    private String dayStr;
-    private String monthStr;
-    private String yearStr;
+    private int hour1;
+    private int day1;
+    private int month1;
+    private int year1;
     private String crimeStr;
     private Date date;
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListner;
+    private String dateStr;
+
     @Nullable
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -75,28 +83,56 @@ public class SubmitCrime extends Fragment  implements AdapterView.OnItemSelected
         crime.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) thisContext);
 
         crimeStr = crime.getSelectedItem().toString();
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                 year1 = cal.get(Calendar.YEAR);
+                 month1 = cal.get(Calendar.MONTH);
+                 day1 = cal.get(Calendar.DAY_OF_MONTH);
+                 //hour1 = cal.get(Calendar.HOUR_OF_DAY);
+                 DatePickerDialog dialog = new DatePickerDialog(thisContext , android.R.style.Widget_Holo_ActionBar_Solid , mDateSetListner, year1 , month1 , day1);
+                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                 dialog.show();
 
-        ArrayAdapter<CharSequence> adaptor2= ArrayAdapter.createFromResource(thisContext, R.array.YY , android.R.layout.simple_spinner_item);
-        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        year.setAdapter(adaptor2);
-        year.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) thisContext);
-        yearStr = year.getSelectedItem().toString();
 
-        ArrayAdapter<CharSequence> adaptor3= ArrayAdapter.createFromResource(thisContext, R.array.MM , android.R.layout.simple_spinner_item);
-        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        month.setAdapter(adaptor3);
-        month.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) thisContext);
-        monthStr = month.getSelectedItem().toString();
 
-        ArrayAdapter<CharSequence> adaptor4= ArrayAdapter.createFromResource(thisContext, R.array.DD , android.R.layout.simple_spinner_item);
-        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        day.setAdapter(adaptor4);
-        day.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) thisContext);
-        dayStr = day.getSelectedItem().toString();
+
+            }
+        });
+        mDateSetListner = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                date.setYear(year);
+                date.setMonth(month);
+                date.setDate(dayOfMonth);
+                month = month+1;
+                dateStr = year + "/" + month + "/" + dayOfMonth;
+                mDisplayDate.setText(dateStr);
+
+            }
+        };
+//        ArrayAdapter<CharSequence> adaptor2= ArrayAdapter.createFromResource(thisContext, R.array.YY , android.R.layout.simple_spinner_item);
+//        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        year.setAdapter(adaptor2);
+//        year.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) thisContext);
+//        yearStr = year.getSelectedItem().toString();
+//
+//        ArrayAdapter<CharSequence> adaptor3= ArrayAdapter.createFromResource(thisContext, R.array.MM , android.R.layout.simple_spinner_item);
+//        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        month.setAdapter(adaptor3);
+//        month.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) thisContext);
+//        monthStr = month.getSelectedItem().toString();
+//
+//        ArrayAdapter<CharSequence> adaptor4= ArrayAdapter.createFromResource(thisContext, R.array.DD , android.R.layout.simple_spinner_item);
+//        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        day.setAdapter(adaptor4);
+//        day.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) thisContext);
+//        dayStr = day.getSelectedItem().toString();
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (yearStr.length()==0 || monthStr.length()==0 || yearStr.length()==0 || hourStr.length() == 0){
+                if (dateStr.length() == 0){
                     Toast.makeText(thisContext , "Date must not be empty" , Toast.LENGTH_LONG).show();
                 }else{
                     if (other.getText().toString().length()==0){
@@ -209,11 +245,12 @@ public class SubmitCrime extends Fragment  implements AdapterView.OnItemSelected
 
         public void findViews(View view) {
         Log.d("taatat", " i am in find view");
-        hour= view.findViewById(R.id.spinner2);
-        day = view.findViewById(R.id.spinner3);
-        month = view.findViewById(R.id.spinner4);
-        year = view.findViewById(R.id.spinner5);
+//        hour= view.findViewById(R.id.spinner2);
+//        day = view.findViewById(R.id.spinner3);
+//        month = view.findViewById(R.id.spinner4);
+//        year = view.findViewById(R.id.spinner5);
         crime =view.findViewById(R.id.spinner1);
+        mDisplayDate = view.findViewById(R.id.select_date);
         submit = view.findViewById(R.id.submit_btn_crrime);
         location = view.findViewById(R.id.location_btn_crime);
         welcome = view.findViewById(R.id.welcome_crime);
